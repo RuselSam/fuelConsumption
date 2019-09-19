eec88f0d9385f1498596379e636a088f22918ee1
ruslan <ruselsam89@gmail.com>
2019-09-19 22:56:11 +0300

    Rename homework file and added wallpaper calculator

--- src/main/java/fuelConsumption.java
+++ src/main/java/fuelConsumption.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class main {
+public class fuelConsumption {
     public static void main(String[] args) {
         Scanner scan=new Scanner(System.in);
         System.out.print("Enter the amount of fuel available: ");
