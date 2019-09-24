d6184d32e859077d04d748a831192c0ace03da30
ruslan <ruselsam89@gmail.com>
2019-09-24 22:48:07 +0300

    fixed #3

--- src/main/java/fuelConsumption.java
+++ src/main/java/fuelConsumption.java
@@ -2,13 +2,13 @@ import java.util.Scanner;
 
 public class fuelConsumption {
     public static void main(String[] args) {
-        Scanner scan=new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);
         System.out.print("Enter the amount of fuel available: ");
-        double fuelVolume=scan.nextDouble(); //volume in liter
+        double fuelVolume = scan.nextDouble(); //volume in liter
         System.out.print("Fuel consumption: ");
-        double fuelConsumption=scan.nextDouble(); //consumption liter per 100 km
-        double distanceToEnd=(fuelVolume/fuelConsumption)*100;
-        int roundedValueDistanceToEnd=(int)distanceToEnd;
-        System.out.println("Distance: "+roundedValueDistanceToEnd);
+        double fuelConsumption = scan.nextDouble(); //consumption liter per 100 km
+        double distanceToEnd = (fuelVolume / fuelConsumption) * 100;
+        int roundedValueDistanceToEnd = (int) distanceToEnd;
+        System.out.println("Distance: " + roundedValueDistanceToEnd);
     }
 }
